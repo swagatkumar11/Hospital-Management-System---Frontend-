5b9e0ea5120e80f6b0e32ee614fdf0e4ceaa0679
swagatkumar11 <swagatkumar2003@gmail.com>
2026-01-21 21:52:42 +0530

    Added Hms frontend folder

---
+++ Hms frontend/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  }
+];
---
+++ Hms frontend/src/app/pages/login/login.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  templateUrl: './login.html',
+  styleUrls: ['./login.css']
+})
+export class LoginComponent {
+}
