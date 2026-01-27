c7e78667e31983c0574952631090bb9c719bd226
swagatkumar11 <swagatkumar2003@gmail.com>
2026-01-27 09:17:11 +0530

    Add frontend

---
+++ Hms frontend/src/app/patient-form/patient-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-patient-form',
+  imports: [],
+  templateUrl: './patient-form.html',
+  styleUrl: './patient-form.css',
+})
+export class PatientForm {
+
+}
